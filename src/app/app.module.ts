cef54ac5d87a083fa6d93eeb2a4572fdef9dbb7a
Shreekrishna <hegdeshreekrishna469@gmail.com>
2025-01-04 20:54:39 +0530

    Lecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './basic/components/login/login.component';
+import { SignupComponent } from './basic/components/signup/signup.component';
+import { provideNzI18n } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    DemoNgZorroAntdModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    provideNzI18n(en_US),
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/company/pages/company-dashboard/company-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-dashboard',
+  standalone: false,
+  
+  templateUrl: './company-dashboard.component.html',
+  styleUrl: './company-dashboard.component.scss'
+})
+export class CompanyDashboardComponent {
+
+}
